f878dab04866c7b498cc2ae336aa6252dc5e6d17
Eugenio Piasini <e.piasini@ucl.ac.uk>
2012-09-11 13:38:00 +0100

    Comparing attenuation in the original (Koen's) model and in my reduced one.
    Important: to get to a meaningful comparison,
    I'm tweaking the spatial discretisation of the original model.

--- neuroConstruct/scripts/attenuation.py
+++ neuroConstruct/scripts/attenuation.py
@@ -17,8 +17,9 @@ project = pm.loadProject(project_file)
 
 #sim_config_name = 'attenuation'
 #conn_name = 'relay_conn'
-sim_config_name = 'attenuation_original'
-conn_name = 'NetConn_relays_Golgi_Vervaeke'
+#sim_config_name = 'attenuation_original'
+#conn_name = 'NetConn_relays_Golgi_Vervaeke'
+sim_config_name = 'attenuation_comparison'
 
 sim_config = project.simConfigInfo.getSimConfig(sim_config_name)
 project.neuronSettings.setNoConsole()
@@ -32,30 +33,33 @@ while pm.isGenerating():
 source_segment_index = 0
 source_fraction_along = 0.5
 delay = 0.
-for target_segment_index in [679,1013,1196]:#[3]:
-    for target_fraction_along in [.5]:
-	sim_ref = timestamp + '_' + str(target_segment_index) + '_' + str(target_fraction_along)
-	sim_path = '../simulations/' + sim_ref
-	project.simulationParameters.setReference(sim_ref)
+for conn_name in ['relay_conn', 'NetConn_relays_Golgi_Vervaeke']:
+    if conn_name == 'relay_conn': target_segments = [4,5,6]
+    else: target_segments = [1526, 1545, 1646]#[679,1013,1196]
+    for target_segment_index in target_segments:
+	for target_fraction_along in [.1]:
+	    sim_ref = timestamp + '_' + str(target_segment_index) + '_' + str(target_fraction_along)
+	    sim_path = '../simulations/' + sim_ref
+	    project.simulationParameters.setReference(sim_ref)
 
-	# delete all existing connections
-	project.generatedNetworkConnections.reset()
+	    # delete all existing connections
+	    project.generatedNetworkConnections.reset()
 
-	# connect the spike relay at the specified point on the golgi dendrite
-	project.generatedNetworkConnections.addSynapticConnection(conn_name, 0, 0, source_segment_index, source_fraction_along, 0, target_segment_index, target_fraction_along, delay, None)
+	    # connect the spike relay at the specified point on the golgi dendrite
+	    project.generatedNetworkConnections.addSynapticConnection(conn_name, 0, 0, source_segment_index, source_fraction_along, 0, target_segment_index, target_fraction_along, delay, None)
 
-	# generate and compile neuron files
-	print "Generating NEURON scripts..."
-	simulator_seed = random.getrandbits(32)
-	project.neuronFileManager.generateTheNeuronFiles(sim_config, None, NeuronFileManager.RUN_HOC,simulator_seed)
-	compile_process = ProcessManager(project.neuronFileManager.getMainHocFile())
-	compile_success = compile_process.compileFileWithNeuron(0,0)
-	# simulate
-	if compile_success:
-	    print "Simulating: simulation reference " + sim_ref
-	    pm.doRunNeuron(sim_config)
-	    timefile_path = sim_path + '/time.dat'
-	    while not os.path.exists(timefile_path):
-		time.sleep(0.1)
+	    # generate and compile neuron files
+	    print "Generating NEURON scripts..."
+	    simulator_seed = random.getrandbits(32)
+	    project.neuronFileManager.generateTheNeuronFiles(sim_config, None, NeuronFileManager.RUN_HOC,simulator_seed)
+	    compile_process = ProcessManager(project.neuronFileManager.getMainHocFile())
+	    compile_success = compile_process.compileFileWithNeuron(0,0)
+	    # simulate
+	    if compile_success:
+		print "Simulating: simulation reference " + sim_ref
+		pm.doRunNeuron(sim_config)
+		timefile_path = sim_path + '/time.dat'
+		while not os.path.exists(timefile_path):
+		    time.sleep(0.1)
 
 System.exit(0)
--- neuroConstruct/scripts/plot_attenuation.py
+++ neuroConstruct/scripts/plot_attenuation.py
@@ -9,21 +9,24 @@ if len(sys.argv) > 1:
 else:
     timestamp = 1347282961.43
 
-target_segment_index_range = [679,1013,1196]#[3]
-target_fraction_along_range = [.5]
+target_fraction_along_range = [.1]
 
 data = []
 
 fig, ax = plt.subplots()
 
-for target_segment_index in target_segment_index_range:
-    for target_fraction_along in target_fraction_along_range:
-	filename = '../simulations/{0}_{1}_{2}/Golgi_Vervaeke_0.dat'.format(timestamp,
-									   target_segment_index,
-									   target_fraction_along)
-	data.append([target_segment_index, target_fraction_along, np.loadtxt(filename)])
-	ax.plot(data[-1][2], label='{0}_{1}'.format(target_segment_index,
-						    target_fraction_along))
+for cell_name in ['reduced', 'Vervaeke']:
+    if cell_name == 'reduced': target_segments = [4,5,6]
+    else: target_segments = [1526, 1545, 1646]
+    for target_segment_index in target_segments:
+	for target_fraction_along in target_fraction_along_range:
+	    filename = '../simulations/{0}_{1}_{2}/Golgi_{3}_0.dat'.format(timestamp,
+									       target_segment_index, target_fraction_along, cell_name)
+	    data.append([target_segment_index,
+			 target_fraction_along,
+			 np.loadtxt(filename)])
+	    ax.plot(data[-1][2], label='{0}_{1}'.format(target_segment_index,
+							target_fraction_along))
 
-ax.legend()
+ax.legend('off')
 plt.show()
