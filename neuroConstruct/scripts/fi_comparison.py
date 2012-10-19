d73fea175a2cfce36bc030e53b0a596894c53d68
Eugenio Piasini <e.piasini@ucl.ac.uk>
2012-10-19 12:05:43 +0100

    Enabled effective remote simulation for fi curve script.

--- neuroConstruct/scripts/fi_comparison.py
+++ neuroConstruct/scripts/fi_comparison.py
@@ -1,6 +1,8 @@
 import os
 import random
 import time
+import subprocess
+from collections import deque
 from java.lang import System
 from java.io import File
 
@@ -29,8 +31,10 @@ pm.doGenerate(sim_config_name, 1234)
 while pm.isGenerating():
     time.sleep(0.02)
 
+sim_refs = deque()
 for amplitude in current_amplitude_range:
-    sim_ref = timestamp + '_' + str(int(round(amplitude)))
+    sim_ref = 'b' + timestamp + '_' + str(int(round(amplitude)))
+    sim_refs.append(sim_ref)
     sim_path = '../simulations/' + sim_ref
     project.simulationParameters.setReference(sim_ref)
     # set current clamp amplitude
@@ -41,6 +45,7 @@ for amplitude in current_amplitude_range:
 	project.elecInputInfo.updateStim(stim)
     # generate and compile neuron files
     print "Generating NEURON scripts..."
+    project.neuronFileManager.setSuggestedRemoteRunTime(10)
     simulator_seed = random.getrandbits(32)
     project.neuronFileManager.generateTheNeuronFiles(sim_config, None, NeuronFileManager.RUN_HOC,simulator_seed)
     compile_process = ProcessManager(project.neuronFileManager.getMainHocFile())
@@ -49,8 +54,27 @@ for amplitude in current_amplitude_range:
     if compile_success:
 	print "Simulating: simulation reference " + sim_ref
 	pm.doRunNeuron(sim_config)
-	timefile_path = sim_path + '/time.dat'
-	while not os.path.exists(timefile_path):
-	    time.sleep(0.1)
+	time.sleep(2)
+	if not sim_config.getMpiConf().isRemotelyExecuted():
+	    # if running locally, never have more than one sim running
+	    # at the same time
+	    print('Simulating on the local machine.')
+	    timefile_path = sim_path + '/time.dat'
+	    while not os.path.exists(timefile_path):
+		time.sleep(2)
 
+while sim_refs and sim_config.getMpiConf().isRemotelyExecuted():
+    # keep on going through the queue of running sims, and try to pull
+    # the results from the cluster
+    sim_ref = sim_refs.popleft()
+    sim_path = '../simulations/' + sim_ref
+    pullsimfile_path = sim_path + '/pullsim.sh'
+    timefile_path = sim_path + '/time.dat'
+    print('Pulling from ' + sim_ref)
+    subprocess.call([pullsimfile_path])
+    if not os.path.exists(timefile_path):
+	sim_refs.append(sim_ref)
+    time.sleep(2)
+
+print('batch reference b' + timestamp)
 System.exit(0)
