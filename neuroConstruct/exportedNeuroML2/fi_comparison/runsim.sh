19b50f933a1c2294e4b149427ec8305d21392cc7
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-12-10 10:52:34 +0000

    Add NeuroML2 simulation for f-i comparison of the three morphologies.

---
+++ neuroConstruct/exportedNeuroML2/fi_comparison/runsim.sh
@@ -0,0 +1,2 @@
+export JNML_HOME=/home/eugenio/src/neuroConstruct/jNeuroMLJar
+/home/eugenio/src/neuroConstruct/jNeuroMLJar/jnml /home/eugenio/phd/nC_projects/GJGolgi_ReducedMorph/neuroConstruct/generatedNeuroML2/LEMS_GJGolgi_ReducedMorph.xml
