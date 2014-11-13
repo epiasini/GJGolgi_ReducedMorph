d19e3a34dc395462a963a2b9c27e9fccb18d2663
Eugenio Piasini <e.piasini@ucl.ac.uk>
2014-11-13 17:36:51 +0000

    Retouch network-based rate IO analysis for inclusion in thesis.

--- neuroConstruct/scripts/rate_IO_network.py
+++ neuroConstruct/scripts/rate_IO_network.py
@@ -14,63 +14,61 @@ from ucl.physiol.neuroconstruct.utils import NumberGenerator
 
 from utils import wait_and_pull_remote
 
+stim_rate_range = range(1., 440., 40.) # neuron doesn't like zero frequency
+
 timestamp = str(time.time())
 pm = ProjectManager(None, None)
 project_path = '../GJGolgi_ReducedMorph.ncx'
 project_file = File(project_path)
 project = pm.loadProject(project_file)
 
-stim_source = 'mf'
-timestamp_prefix = 'net_' + stim_source
-
-sim_config_name = 'network_IO_' + stim_source
-sim_config = project.simConfigInfo.getSimConfig(sim_config_name)
-project.neuronSettings.setNoConsole()
-
-stim_rate_range = range(1., 440., 40.) # neuron doesn't like zero frequency
-
-
-# generate
-pm.doGenerate(sim_config_name, 1234)
-while pm.isGenerating():
-    time.sleep(0.02)
-print('network generated')
-
 sim_refs = []
-for rate in stim_rate_range:
-    sim_ref = timestamp_prefix + timestamp + '_' + str(int(round(rate)))
-    sim_refs.append(sim_ref)
-    sim_path = '../simulations/' + sim_ref
-    project.simulationParameters.setReference(sim_ref)
-    # set stim rate
-    rate_in_kHz = rate/1000.
-    for cell_type in ['Vervaeke', 'reduced']:
-	stim = project.elecInputInfo.getStim('network_' + cell_type + '_' +
-					     stim_source + '_AMPAonly')
-	stim.setRate(NumberGenerator(rate_in_kHz))
-	project.elecInputInfo.updateStim(stim)
-    # generate and compile neuron files
-    print "Generating NEURON scripts..."
-    project.neuronFileManager.setSuggestedRemoteRunTime(30)
-    simulator_seed = random.getrandbits(32)
-    project.neuronFileManager.generateTheNeuronFiles(sim_config, None, NeuronFileManager.RUN_HOC,simulator_seed)
-    compile_process = ProcessManager(project.neuronFileManager.getMainHocFile())
-    compile_success = compile_process.compileFileWithNeuron(0,0)
-    # simulate
-    if compile_success:
-	print "Submitting simulation reference " + sim_ref
-	pm.doRunNeuron(sim_config)
-	time.sleep(5) # Wait for sim to be kicked off
-	if not sim_config.getMpiConf().isRemotelyExecuted():
-	    # if running locally, never have more than one sim running
-	    # at the same time
-	    print('Simulating on the local machine.')
-	    timefile_path = sim_path + '/time.dat'
-	    while not os.path.exists(timefile_path):
-		time.sleep(5)
+for stim_source in ['mf', 'pf']:
+    timestamp_prefix = 'net_' + stim_source
+
+    sim_config_name = 'network_IO_' + stim_source
+    sim_config = project.simConfigInfo.getSimConfig(sim_config_name)
+    project.neuronSettings.setNoConsole()
+
+    # generate
+    pm.doGenerate(sim_config_name, 1234)
+    while pm.isGenerating():
+        time.sleep(0.02)
+    print('network generated')
+    for rate in stim_rate_range:
+        sim_ref = timestamp_prefix + timestamp + '_' + str(int(round(rate)))
+        sim_refs.append(sim_ref)
+        sim_path = '../simulations/' + sim_ref
+        project.simulationParameters.setReference(sim_ref)
+        # set stim rate
+        rate_in_kHz = rate/1000.
+        for cell_type in ['Vervaeke', 'reduced']:
+            stim = project.elecInputInfo.getStim('network_' + cell_type + '_' +
+                                                 stim_source + '_AMPAonly')
+            stim.setRate(NumberGenerator(rate_in_kHz))
+            project.elecInputInfo.updateStim(stim)
+        # generate and compile neuron files
+        print "Generating NEURON scripts..."
+        project.neuronFileManager.setSuggestedRemoteRunTime(30)
+        simulator_seed = random.getrandbits(32)
+        project.neuronFileManager.generateTheNeuronFiles(sim_config, None, NeuronFileManager.RUN_HOC,simulator_seed)
+        compile_process = ProcessManager(project.neuronFileManager.getMainHocFile())
+        compile_success = compile_process.compileFileWithNeuron(0,0)
+        # simulate
+        if compile_success:
+            print "Submitting simulation reference " + sim_ref
+            pm.doRunNeuron(sim_config)
+            time.sleep(5) # Wait for sim to be kicked off
+            if not sim_config.getMpiConf().isRemotelyExecuted():
+                # if running locally, never have more than one sim running
+                # at the same time
+                print('Simulating on the local machine.')
+                timefile_path = sim_path + '/time.dat'
+                while not os.path.exists(timefile_path):
+                    time.sleep(5)
 
 if sim_config.getMpiConf().isRemotelyExecuted():
     wait_and_pull_remote(sim_refs)
 
-print('batch reference ' + timestamp_prefix + timestamp)
+print('batch reference net_' + timestamp)
 System.exit(0)
--- neuroConstruct/scripts/rate_IO_network_plot.py
+++ neuroConstruct/scripts/rate_IO_network_plot.py
@@ -1,59 +1,88 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-## Network I/O plotting script.
+## Network I/O plotting script, looking at basolateral or apical
+## stimulation at a subset of cells, and how this propagates through
+## gap junctions to cells that are not directly innervated.
+## usage: pythonv rate_IO_network_plot.py net_1415883300.46
 import sys
 import numpy as np
-from matplotlib import pyplot as plt
 
-timestamp = sys.argv[1]
+import matplotlib
+import seaborn as sns
+rc = matplotlib.rc_params_from_file('/home/ucbtepi/thesis/matplotlibrc.thesis',
+                                    use_default_template=False)
+matplotlib.rcParams.update(rc)
+
+figsize = (2.5, 3)
+
+batch_ref = sys.argv[1]
 stim_rate_range = range(1, 440, 40)
+n_points = len(stim_rate_range)
 
-inn_fig, inn_ax = plt.subplots()
-ninn_fig, ninn_ax = plt.subplots()
+fig, ax = matplotlib.pyplot.subplots((1,2), figsize=figsize)
 
-cell_types = ['reduced', 'Vervaeke']
-rates = {}
-sigmas = {}
+cell_types = ['Vervaeke', 'reduced']
+colors = {'Vervaeke': '#55A868', 'reduced': '#4C72B0'}
 threshold = 'min20'
 
-for cell_type in cell_types:
-    rates[cell_type] = {'innervated':[], 'non_innervated':[]}
-    sigmas[cell_type] = {'innervated':[], 'non_innervated':[]}
-    for amplitude in stim_rate_range:
-	innervated_rates = []
-	non_innervated_rates = []
-	for cell_n in range(45):
-	    filename='../simulations/{0}_{1}/Golgi_network_{2}_{3}.SPIKES_{4}.spike'.format(timestamp,
-											    amplitude,
-											    cell_type,
-											    cell_n,
-											    threshold)
-	    try:
-		cell_rate = len(np.loadtxt(filename))
-	    except TypeError:
-		cell_rate = 0
-	    if cell_n < 15:
-		innervated_rates.append(cell_rate)
-	    else:
-		non_innervated_rates.append(cell_rate)
-	innervated_rates = np.array(innervated_rates)
-	non_innervated_rates = np.array(non_innervated_rates)
-	rates[cell_type]['innervated'].append(innervated_rates.mean())
-	rates[cell_type]['non_innervated'].append(non_innervated_rates.mean())
-	sigmas[cell_type]['innervated'].append(innervated_rates.std())
-	sigmas[cell_type]['non_innervated'].append(non_innervated_rates.std())
-    inn_ax.errorbar(stim_rate_range, rates[cell_type]['innervated'],
-		    yerr=sigmas[cell_type]['innervated'], label=cell_type, fmt='o-')
-    ninn_ax.errorbar(stim_rate_range, rates[cell_type]['non_innervated'],
-		    yerr=sigmas[cell_type]['non_innervated'], label=cell_type, fmt='o-')
+zorder_counter = 1
+lines = []
+
+for stim_source, lin zip(['pf'], ['-']):
+    ax.set_color_cycle(None) # reset color cycle
+    for cell_type in cell_types:
+        color = colors[cell_type]
+        rates = {'innervated': np.zeros(n_points), 'non_innervated': np.zeros(n_points)}
+        sigmas = {'innervated': np.zeros(n_points), 'non_innervated': np.zeros(n_points)}
+        for i, amplitude in enumerate(stim_rate_range):
+            sim_ref = 'net_{}{}_{}'.format(stim_source,
+                                           batch_ref.lstrip('net_'),
+                                           amplitude)
+            innervated_rates = []
+            non_innervated_rates = []
+            for cell_n in range(45):
+                filename='../simulations/{0}/Golgi_network_{1}_{2}.SPIKES_{3}.spike'.format(sim_ref, cell_type, cell_n, threshold)
+                try:
+                    cell_rate = len(np.loadtxt(filename))
+                except TypeError:
+                    cell_rate = 0
+                if cell_n < 15:
+                    innervated_rates.append(cell_rate)
+                else:
+                    non_innervated_rates.append(cell_rate)
+            innervated_rates = np.array(innervated_rates)
+            non_innervated_rates = np.array(non_innervated_rates)
+            rates['innervated'][i] = innervated_rates.mean()
+            rates['non_innervated'][i] = non_innervated_rates.mean()
+            sigmas['innervated'][i] = innervated_rates.std()
+            sigmas['non_innervated'][i] = non_innervated_rates.std()
+        ax.plot(stim_rate_range, rates['innervated'], label="{}".format(cell_type), linestyle=linestyle, linewidth=1.5, color=color)
+        ax.fill_between(stim_rate_range, rates['innervated']-sigmas['innervated'], rates['innervated']+sigmas['innervated'], color=color, alpha=0.3)
+        ax.plot(stim_rate_range, rates['non_innervated'], linestyle="--", linewidth=1.5, dashes=(4,1.5), color=color)
+        ax.fill_between(stim_rate_range, rates['non_innervated']-sigmas['non_innervated'], rates['non_innervated']+sigmas['non_innervated'], color=color, alpha=0.3)
+        # ax.errorbar(stim_rate_range, rates[cell_type]['innervated'],
+        #             yerr=sigmas[cell_type]['innervated'],
+        #             label="{}, innervated".format(cell_type), fmt=linestyle, zorder=zorder_counter, capsize=1.5, capthick=0.5, elinewidth=1, alpha=0.7)
+        # ax.errorbar(stim_rate_range, rates[cell_type]['non_innervated'],
+        #             yerr=sigmas[cell_type]['non_innervated'],
+        #             label="{}, non innervated".format(cell_type), fmt=linestyle, zorder=zorder_counter+1, capsize=1.5, capthick=0.5, elinewidth=1, alpha=0.7)
+        zorder_counter += 1
 
 
-inn_ax.legend(loc='best')
-inn_ax.set_xlabel('stimulation rate (Hz)')
-inn_ax.set_ylabel('firing rate (Hz)')
-inn_fig.suptitle('Direct innervated GoCs')
-ninn_ax.legend(loc='best')
-ninn_ax.set_xlabel('stimulation rate (Hz)')
-ninn_ax.set_ylabel('firing rate (Hz)')
-ninn_fig.suptitle('Non innervated GoCs')
-plt.show()
+ax.legend(loc='center right')
+ax.set_xlabel('Stimulation rate (Hz)')
+ax.set_ylabel('Firing rate (Hz)')
+#fig.suptitle('Direct innervated GoCs')
+ax.locator_params(axis='x', tight=False, nbins=4)
+ax.locator_params(axis='y', tight=False, nbins=4)
+ax.set_xlim((-10, 410))
+ax.set_ylim((0, 80))
+matplotlib.pyplot.tight_layout()
+
+if not 10 in ax.get_yticks():
+    ax.set_yticks(np.hstack((ax.get_yticks(), 10)))
+
+fig_filename = 'fig/rate_IO_network.pdf'
+fig.savefig(fig_filename)
+print("result saved to {}".format(fig_filename))
+matplotlib.pyplot.show()
